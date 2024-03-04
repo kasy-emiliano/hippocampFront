73332ec03ed50f534ebda79ea46ad8b72b4b1e0f
kasy-emiliano <99735371+kasy-emiliano@users.noreply.github.com>
2024-03-04 13:52:45 +0300

    admin

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,7 +10,7 @@ const Header = () => {
 
     <nav class="bg-gray-800 border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
         <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-            <Link to="https://flowbite.com" class="flex items-center">
+            <Link to="signinadmin" class="flex items-center">
                 <img src={Logo2} class="mr-3 h-8 w-18 sm:h-9" alt="Flowbite Logo" />
             </Link>
             <div class="flex items-center lg:order-2">
